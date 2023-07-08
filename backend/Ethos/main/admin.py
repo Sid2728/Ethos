b4517c24ca2dc7304f11e578543057a09bf85c23
Siddhant <103687316+Sid2728@users.noreply.github.com>
2023-07-09 03:00:08 +0530

    Delete backend/Ethos directory

--- backend/Ethos/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from main.models import Audio,TimeStamp,upload
-
-# Register your models here.
-admin.site.register(Audio)
-admin.site.register(TimeStamp)
-admin.site.register(upload)
